ae182a58e4be0253479c7e7ba6440109bc04af3d
Eric Slyman <slymane@wwu.edu>
2020-03-12 17:30:59 -0700

    Make JS/WebGL sample interactive on webpage

---
+++ docs/js/ray1.js
@@ -0,0 +1,64 @@
+var Ray1 = function (canvas, gl) {
+    this.cameraAngle = 0;
+
+    var vertexSourceFile = "shaders/vertex.glsl";
+    var fragmentSourceFile = "shaders/fragment.glsl";
+
+    this.indexCount = ViewIndices.length;
+    this.positionVbo = createVertexBuffer(gl, ViewPositions);
+    this.indexIbo = createIndexBuffer(gl, ViewIndices);
+    this.shaderProgram = createShaderProgram(gl, vertexSourceFile, fragmentSourceFile);
+
+    this.x = 0;
+    this.y = 0;
+    gl.enable(gl.DEPTH_TEST);
+}
+
+Ray1.prototype.render = function (canvas, gl, w, h) {
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // this.cubeMesh.renderRay(gl, w, h);
+
+    // Set Shader program
+    gl.useProgram(this.shaderProgram);
+
+    // ##### ADD STUFF HERE
+
+    // IMPORTANT: OpenGL has different matrix conventions than our JS program. We need to transpose the matrix before passing it
+    // to OpenGL to get the correct matrix in the shader.
+    //gl.uniformMatrix4fv(gl.getUniformLocation(this.shaderProgram, "ModelViewProjection"), 
+    //    false, modelViewProjection.transpose().m);
+
+    var d = new Date();
+    var s = d.getSeconds() + (d.getMilliseconds() / 1000)
+    var t = (Math.PI / 30) * ((10 * s) % 60);
+    gl.uniform1f(gl.getUniformLocation(this.shaderProgram, "WindowHeight"), h)
+    gl.uniform1f(gl.getUniformLocation(this.shaderProgram, "WindowWidth"), w)
+    gl.uniform1f(gl.getUniformLocation(this.shaderProgram, "time"), t);
+    gl.uniform1f(gl.getUniformLocation(this.shaderProgram, "MouseX"), this.x);
+    gl.uniform1f(gl.getUniformLocation(this.shaderProgram, "MouseY"), this.y);
+    // #### END
+
+    // Bind index, position, and normal buffers
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexIbo);
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.positionVbo);
+    var positionAttrib = gl.getAttribLocation(this.shaderProgram, "aPosition");
+    if (positionAttrib >= 0) {
+        gl.enableVertexAttribArray(positionAttrib);
+        gl.vertexAttribPointer(positionAttrib, 3, gl.FLOAT, false, 0, 0);
+    }
+
+    // Draw...
+    gl.drawElements(gl.TRIANGLES, this.indexCount, gl.UNSIGNED_SHORT, 0);
+}
+
+Ray1.prototype.dragCamera = function (dy) {
+    this.cameraAngle = Math.min(Math.max(this.cameraAngle - dy * 0.5, -90), 90);
+}
+
+Ray1.prototype.setLight = function (canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    this.x = 100*(((evt.clientX - rect.left) / rect.width) - 0.5);
+    this.y = 100*(((evt.clientY - rect.top) / rect.height) - 0.5);
+}
---
+++ docs/shaders/fragment.glsl
@@ -0,0 +1,124 @@
+#version 300 es
+#define FLT_MAX 3.402823466e+38
+
+precision mediump float;
+out vec4 fragmentColor;
+
+uniform float WindowHeight;
+uniform float WindowWidth;
+uniform float time;
+uniform float MouseX;
+uniform float MouseY;
+
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+struct Sphere {
+    vec3 center;
+    float radius;
+    vec3 color;
+};
+
+struct PointLight {
+    float intensity;
+    vec3 position;
+};
+
+struct CanonicalCamera {
+    float canv_height;
+    float canv_width;
+};
+
+struct HitRecord {
+    bool valid;
+    float t;
+    vec3 intersection;
+    vec3 normal;
+    Sphere object;
+};
+
+struct Scene {
+    vec3 background;
+    Sphere object;
+    PointLight light;
+    float ambient;
+};
+
+vec3 shade_light(Ray ray, HitRecord hitrec, Scene scene) {
+    vec3 l = normalize(scene.light.position - hitrec.intersection);
+    vec3 k_d = hitrec.object.color;
+    return scene.ambient + k_d * scene.light.intensity * max(0.0, dot(hitrec.normal, l));
+}
+
+Ray pixel_to_ray(CanonicalCamera camera, float i, float j) {
+    float vp_height = camera.canv_height / camera.canv_width;
+    float u =  ((j - 0.5)    / camera.canv_width       - 0.5);
+    float v = -((i - 0.5)    / camera.canv_height      - 0.5) * vp_height;
+    float w = -1.0;
+
+    return Ray(vec3(0, 0, 0), normalize(vec3(u, v, w)));
+}
+
+HitRecord ray_intersect(Ray ray, Sphere object) {
+    vec3 dist = ray.origin - object.center;
+    float B = dot(ray.direction, dist);
+    float C = dot(dist, dist) - pow(object.radius, 2.0);
+
+    float t;
+    bool valid;
+    float delta = pow(B, 2.0) - C;
+    if (delta > 0.0) {
+        delta = sqrt(delta);
+        float pos = -B + delta;
+        float neg = -B - delta;
+        t = min(pos, neg);
+        valid = true;
+    } else if (delta == 0.0) {
+        t = -B;
+        valid = true;
+    } else {
+        t = 0.0;
+        valid = false;
+    }
+
+    vec3 intersection = ray.origin + ray.direction * t;
+    vec3 normal = normalize(intersection - object.center);
+
+    return HitRecord(valid, t, intersection, normal, object);
+}
+
+HitRecord closest_intersect(Ray ray, Sphere object, float tmin, float tmax) {
+    HitRecord hr = ray_intersect(ray, object);
+    
+    if (hr.t < tmin || hr.t > tmax) {
+        hr.valid = false;
+    }
+
+    return hr;
+}
+
+vec3 traceray(Scene scene, Ray ray, float tmin, float tmax) {
+    HitRecord hr = closest_intersect(ray, scene.object, tmin, tmax);
+
+    if (hr.valid) {
+        return shade_light(ray, hr, scene);
+    }
+    return scene.background;
+}
+
+Scene get_scene1() {
+    vec3 bg = vec3(0.95, 0.95, 0.95);
+    Sphere object = Sphere(vec3(0, 0, -7), 1.0, vec3(pow(sin(time), 2.0), 0.5, pow(cos(time), 2.0)));
+    PointLight light = PointLight(1.0, vec3(MouseX, MouseY, 0));
+    return Scene(bg, object, light, 0.0);
+}
+
+void main() {
+    Scene scene = get_scene1();
+    CanonicalCamera camera = CanonicalCamera(WindowHeight, WindowWidth);
+    Ray ray = pixel_to_ray(camera, gl_FragCoord.y, gl_FragCoord.x);
+    vec3 color = traceray(scene, ray, 1.0, FLT_MAX);
+    fragmentColor = vec4(color, 1.0);
+}
